8f94cf208e715a4cd8d5f8a560505dbf72ee587e
snackeye1 <aristotle.maule@hotmail.com>
2021-03-31 16:23:41 -0400

    app logic added

--- grocery-bud/src/components/GroceryBud.js
+++ grocery-bud/src/components/GroceryBud.js
@@ -1,10 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './GroceryBud.css';
 
-const groceryList = ["Eggs", "Bacon"];
-
 const GroceryBud = () => {
-    const clearItemsButton = <button>Clear Items</button>;
+    const [enteredItem, setEnteredItem] = useState('Shampoo');
+    const [groceryList, setGroceryList] = useState(["Eggs", "Bacon"]);
+    // False for creating grocery item, true for editing
+    const [inputType, setInputType] = useState(false);
+    const [indexOfEditItem, setIndexOfEditItem] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //New item
+        if(inputType === false) {
+            setGroceryList(groceryList => [...groceryList, enteredItem]);
+            setEnteredItem('');
+            console.log("item added to list");
+        }
+        // Updating Item
+        else if(inputType === true) {
+            let groceryArray = [...groceryList];
+            groceryArray[indexOfEditItem] = enteredItem;
+            setGroceryList(groceryArray);
+            setIndexOfEditItem(null);
+            setInputType(false);
+            setEnteredItem('');
+        } 
+        else {
+            console.log("There was a problem");
+        }
+    };
+
+    const removeItem = (index) => {
+        let groceryArray = [...groceryList];
+        if(index >= 0 && index < groceryArray.length) {
+            groceryArray.splice(index, 1);
+            setGroceryList(groceryArray);
+        } else {
+            console.log("error");
+        }
+    };
+
+    // Handles button click of edit item icon, updating of grocery array is only done with handleInput
+    const editItem = (index) => {
+        console.log(index);
+        setInputType(true);
+        setEnteredItem(groceryList[index]);
+        setIndexOfEditItem(index);
+    };
+    const clearItems = () => {
+        setGroceryList([]);
+    };
+
+
+    const clearItemsButton = () => { 
+        let groceryArray = [...groceryList]
+        if([groceryArray.length] > 0) {
+            return <button onClick={clearItems}>Clear Items</button>;
+        } else {
+            return <span></span>;
+        }
+    }
+
+    const inputFieldDisplay = () => {
+        if(inputType === true) {
+            return <button className="edit-btn" type="submit">Edit</button>;
+        } 
+        else if(inputType === false) {
+            return <button type="submit">Submit</button>;
+        } 
+    };
 
     const renderedItems = groceryList.map(( item, index ) => {
         return( 
@@ -13,8 +77,8 @@ const GroceryBud = () => {
                     {item}
                 </div>
                 <div className="icons">
-                    <i className="edit icon"></i>
-                    <i className="trash alternate icon"></i>
+                    <i onClick={() => editItem(index)} className="edit icon"></i>
+                    <i onClick={() => removeItem(index)} className="trash alternate icon"></i>
                 </div>
             </div>
         );
@@ -23,15 +87,19 @@ const GroceryBud = () => {
     return (
         <div className="grocery-bud">
             <h2 className="grocery-bud-title">Grocery Bud</h2>
-            <div className="grocery-bud-input">
-                <input type="text" />
-                <button>Submit</button>
-            </div>
+            <form onSubmit={(e) => handleSubmit(e)} className="grocery-bud-input">
+                <input 
+                    type="text" 
+                    value={enteredItem}
+                    onChange={(e) => setEnteredItem(e.target.value)}
+                />
+                {inputFieldDisplay()}
+            </form>
             <div className="grocery-list">
                 {renderedItems}
             </div>
             <div className="clear-items-button">
-                {clearItemsButton}
+                {clearItemsButton()}
             </div>
         </div>
     );
