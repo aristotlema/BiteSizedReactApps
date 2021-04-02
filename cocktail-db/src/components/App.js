f2e46094c14c451f1b58a989ebb0b79e5a7f7fbd
snackeye1 <aristotle.maule@hotmail.com>
2021-04-02 11:51:33 -0400

    init commit, SCSS setup, basic styling

---
+++ cocktail-db/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './reset.css';
+import './App.scss';
+import NavBar from './NavBar';
+
+const App = () => {
+    return (
+        <NavBar />
+    );
+};
+
+export default App;
---
+++ cocktail-db/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NavBar.scss';
+
+const NavBar = () => {
+    return(
+        <nav className="nav-bar">
+            <h3 className="nav-bar-title">The <span className="title-highlight">Cocktail</span> DB</h3>
+            <div className="nav-bar-links">
+                <a>Home</a>
+                <a>About</a>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
