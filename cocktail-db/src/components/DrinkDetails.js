62af078c9dec9ae74d30f716fc9c0349d6e8e96b
snackeye1 <aristotle.maule@hotmail.com>
2021-04-05 08:22:48 -0400

    drink details page built out and styled

--- cocktail-db/src/components/DrinkDetails.js
+++ cocktail-db/src/components/DrinkDetails.js
@@ -3,8 +3,27 @@ import Link from './Link';
 import Button from './Button';
 import './DrinkDetails.scss';
 
-const DrinkDeatails = ({ selectedCocktail }) => {
+const DrinkDetails = ({ selectedCocktail }) => {
     console.log(selectedCocktail);
+    
+    const renderIngredients = () => {
+        let rows = []
+        for(let i = 1; i <= 15; i++){
+            let currentMeasure = selectedCocktail[`strMeasure${i}`];
+            let currentIngredient = selectedCocktail[`strIngredient${i}`];
+            // Needed this refactor because some have empty strings as ingredients 
+            // examine JSON for idDRink 178343 - Michelada for example of thisd
+            let isNull = currentIngredient === null && currentMeasure === null;
+            let isEmptyString = currentIngredient === "" && currentMeasure === "";
+
+            if(isNull || isEmptyString) {
+                break;
+            } else {
+                rows.push(<div key={i}>{currentMeasure} - {currentIngredient}</div>);
+            }
+        }
+        return <div>{rows}</div>;
+    };
     return (
         <section className="drink-details">
             <Link href="/">
@@ -14,9 +33,22 @@ const DrinkDeatails = ({ selectedCocktail }) => {
             <div className="drink-details-image">
                 <img src={selectedCocktail.strDrinkThumb} alt={selectedCocktail.strDrink} />
             </div>
+            <div className="drink-details-body">
+                <div><label for="Name" className="high-light">Name: </label> {selectedCocktail.strDrink}</div>
+                <div><label for="Category" className="high-light">Category: </label> {selectedCocktail.strCategory}</div>
+                <div><label for="Info" className="high-light">Info: </label> {selectedCocktail.strAlcoholic}</div>
+                <div><label for="Glass" className="high-light">Glass: </label> {selectedCocktail.strGlass}</div>
+                <div><label for="Instructions" className="high-light">Instructions: </label> {selectedCocktail.strInstructions}</div>
+                
+
+                <h2 className="ingredients"><span className="high-light">Ingredients</span></h2>
+                <div>
+                    {renderIngredients()}
+                </div>
+            </div>
         </section>
         
     );
 };  
 
-export default DrinkDeatails;
+export default DrinkDetails;
--- cocktail-db/src/components/DrinkList.js
+++ cocktail-db/src/components/DrinkList.js
@@ -13,13 +13,13 @@ const DrinkList = ({ cocktailResults, details }) => {
         if(!cocktailResults) {
             return <h1>No results found</h1>;
         } else if (cocktailResults.length > 0) {
-            return cocktailResults.map(({ idDrink, strDrinkThumb, strDrink, strGlass }) => {
+            return cocktailResults.map(({ idDrink, strDrinkThumb, strDrink, strCategory }) => {
                 return (
                     <div onClick={(e) => loadDetails(e, idDrink)} key={idDrink} className="drink-list-card">
                         <Link href={`/details/${idDrink}`}>
                             <img src={strDrinkThumb} alt={strDrink} className="drink-list-card-image"/>
                             <div className="drink-list-card-body">
-                                <div className="card-title">{strGlass}</div>
+                                <div className="card-title">{strCategory}</div>
                                 <div className="card-drink-name">{strDrink}</div>
                                 <button className="card-button">Details</button>
                             </div>
@@ -30,7 +30,7 @@ const DrinkList = ({ cocktailResults, details }) => {
         } else {
             return <h1>Something went wrong</h1>;
         }
-    }
+    };
 
     return (
         <section className="drink-list">
