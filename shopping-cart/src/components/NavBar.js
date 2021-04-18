a30c8941be234cfac39d581dd95cbdb200c1aa32
aristotlema <aristotle.maule@hotmail.com>
2021-04-18 15:25:14 -0400

    init commit

---
+++ shopping-cart/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import './NavBar.scss';
+
+const NavBar = () => {
+    return (
+        <nav className="nav-bar">
+            <h2>Shopping Cart</h2>
+            <h4>Cart</h4>
+        </nav>
+    )
+};
+
+export default NavBar;
---
+++ shopping-cart/src/reducers/index.js
@@ -0,0 +1,26 @@
+import { combineReducers } from 'redux';
+
+const itemsReducer = () => {
+    return [
+        {
+            model: 'IPhone 11',
+            price: 699,
+            image: ''
+        },
+        {
+            model: 'IPhone 11',
+            price: 699,
+            image: ''
+        },
+        {
+            model: 'IPhone 11',
+            price: 699,
+            image: ''
+        },
+        {
+            model: 'IPhone 11',
+            price: 699,
+            image: ''
+        }
+    ];
+};
